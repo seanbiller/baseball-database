303a933834c4a48a18d1453a24071680a58926af
Sean Biller <seanmbiller@gmail.com>
2019-10-29 23:37:47 -0400

    Extra Credit and Answer

--- queries.sql
+++ queries.sql
@@ -1,5 +1,45 @@
-UPDATE teams SET abbreviation = 'WAS' WHERE abbreviation = 'WSH';
+USE baseball;
+    
+SELECT *
+FROM   players
+       JOIN teams
+         ON players.currentteamid = teams.id 
+WHERE  teams.mascot = 'Red Sox'; 
 
-SELECT startTime, homeScore, awayScore FROM games WHERE startTime >= '2019-06-06 00:00:00' AND startTime <= '2019-06-06 17:00:00';
+SELECT 
+firstName, lastName, position, abbreviation, homeRuns
+FROM   players
+       JOIN teams
+         ON players.currentteamid = teams.id 
+       JOIN hittingstats 
+         ON players.id = hittingstats.playerid 
+       JOIN games
+         ON hittingstats.gameid = games.id 
+WHERE games.startTime LIKE '2019-06-06%'
+ORDER  BY hittingstats.homeruns DESC LIMIT  5;
 
-SELECT * FROM teams WHERE league = 'NL';
+SELECT 
+firstName, lastName, position, abbreviation, earnedRuns
+FROM   players
+       JOIN teams
+         ON players.currentteamid = teams.id 
+       JOIN pitchingstats 
+         ON players.id = pitchingstats.playerid 
+       JOIN games
+         ON pitchingstats.gameid = games.id 
+WHERE games.startTime LIKE '2019-06-06%'
+ORDER  BY pitchingstats.earnedRuns DESC LIMIT  5;
+
+SELECT 
+firstName, lastName, position, abbreviation, doubles, startTime, homeTeamId, awayTeamId
+FROM   players
+       JOIN teams
+         ON players.currentteamid = teams.id 
+       JOIN hittingStats 
+         ON players.id = hittingStats.playerid 
+       JOIN games
+         ON hittingStats.gameid = games.id 
+WHERE games.startTime LIKE '2019-06-06%'
+ORDER  BY hittingStats.doubles DESC LIMIT  5;
+ 
+    
